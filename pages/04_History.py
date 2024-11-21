6634a8cf9fe1e4484af00a45d7e4c7a4efcd5a78
Lorenzo Flores (Lj) <47231323+ljyflores@users.noreply.github.com>
2024-11-20 21:51:36 -0500

    Undo wrapping in main

--- pages/04_History.py
+++ pages/04_History.py
@@ -8,60 +8,53 @@ from utils import (
 )
 
 
-def main():
-    # Read in conversation history if available, otherwise create a new dictionary
-    if os.path.exists("assets/history.json"):
-        history_list = read_list_of_dicts_from_json("assets/history.json")
-    else:
-        history_list = list[dict[str, str]]()
+# Read in conversation history if available, otherwise create a new dictionary
+if os.path.exists("assets/history.json"):
+    history_list = read_list_of_dicts_from_json("assets/history.json")
+else:
+    history_list = list[dict[str, str]]()
 
-    # Name the pages and tab bar
-    st.set_page_config(page_title="Hello", page_icon="👋")
+# Name the pages and tab bar
+st.set_page_config(page_title="Hello", page_icon="👋")
 
-    # Initialize floating features for the interface
-    float_init()
+# Initialize floating features for the interface
+float_init()
 
-    st.title("History Mode")
-    if history_list:
-        print(history_list)
-        num_suggestions = 0
-        num_feedback = 0
+st.title("History Mode")
+if history_list:
+    print(history_list)
+    num_suggestions = 0
+    num_feedback = 0
 
-        for item in history_list:
-            if item["role"] == "User" or item["role"] == "Partner Speaker":
-                st.markdown("---")
-                st.write(f"#### :green[**{item['role']}**]: {item['content']}")  # type: ignore
+    for item in history_list:
+        if item["role"] == "User" or item["role"] == "Partner Speaker":
+            st.markdown("---")
+            st.write(f"#### :green[**{item['role']}**]: {item['content']}")  # type: ignore
 
-            elif item["role"] == "Feedback LLM":
-                num_feedback += 1
-                st.write(f"#### :blue[**{item['role']}**]:")
-                for t in item["content"].split("\n"):
-                    st.write(f"#### {t}")
-                if st.button("Advanced Feedback", key=f"feedback{num_feedback}"):
-                    message = [{"role": "user", "content": item["content"]}]
-                    llm_advanced_feedback = (
-                        get_feedback_answer_advanced(message, "gpt-4o-mini")
-                        or "No response given!"
-                    )
-                    st.markdown(
-                        f"#### :blue[**{item['role']}**]: {llm_advanced_feedback}"
-                    )
+        elif item["role"] == "Feedback LLM":
+            num_feedback += 1
+            st.write(f"#### :blue[**{item['role']}**]:")
+            for t in item["content"].split("\n"):
+                st.write(f"#### {t}")
+            if st.button("Advanced Feedback", key=f"feedback{num_feedback}"):
+                message = [{"role": "user", "content": item["content"]}]
+                llm_advanced_feedback = (
+                    get_feedback_answer_advanced(message, "gpt-4o-mini")
+                    or "No response given!"
+                )
+                st.markdown(f"#### :blue[**{item['role']}**]: {llm_advanced_feedback}")
 
-            elif item["role"] == "Suggestion LLM":
-                num_suggestions += 1
-                st.write(f"#### :blue[**{item['role']}**]:")
-                for t in item["content"].split("\n"):
-                    st.write(f"#### {t}")
-                if st.button("Advanced Suggestion", key=f"suggestion{num_suggestions}"):
-                    message = [{"role": "user", "content": item["content"]}]
-                    llm_advanced_suggestions = (
-                        get_suggestion_answer_advanced(message, "gpt-4o-mini")
-                        or "No response given!"
-                    )
-                    st.write(f"#### :blue[**{item['role']}**]: {llm_advanced_suggestions}")  # type: ignore
-    else:
-        st.write("No conversation history to show!")  # type: ignore
-
-
-if __name__ == "__main__":
-    main()
+        elif item["role"] == "Suggestion LLM":
+            num_suggestions += 1
+            st.write(f"#### :blue[**{item['role']}**]:")
+            for t in item["content"].split("\n"):
+                st.write(f"#### {t}")
+            if st.button("Advanced Suggestion", key=f"suggestion{num_suggestions}"):
+                message = [{"role": "user", "content": item["content"]}]
+                llm_advanced_suggestions = (
+                    get_suggestion_answer_advanced(message, "gpt-4o-mini")
+                    or "No response given!"
+                )
+                st.write(f"#### :blue[**{item['role']}**]: {llm_advanced_suggestions}")  # type: ignore
+else:
+    st.write("No conversation history to show!")  # type: ignore
